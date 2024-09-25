d4f039c41938c03f304a3678c9a0d1e2e9436502
Arkadiusz4 <arekmika4@gmail.com>
2024-09-25 14:32:57 +0200

    Update for getting values of all companies

--- yahoo-fetcher/sample_api_fetcher.py
+++ yahoo-fetcher/sample_api_fetcher.py
@@ -1,52 +1,49 @@
-# from flask import Flask, jsonify, request
-# from fetcher import Fetcher
-#
-# app = Flask(__name__)
-#
-# @app.route('/historical_data', methods=['GET'])
-# def get_historical_data():
-#     ticker = request.args.get('ticker', default='AAPL', type=str)
-#     period = request.args.get('period', default='1y', type=str)
-#     interval = request.args.get('interval', default='1d', type=str)
-#
-#     fetcher = Fetcher(ticker)
-#     data = fetcher.get_historical_data(period=period, interval=interval)
-#
-#     if data is not None:
-#         # Konwersja indeksu (Timestamp) na string, aby uniknąć błędów
-#         data.index = data.index.strftime('%Y-%m-%d')
-#         return jsonify(data.to_dict(orient='index'))  # Przekazanie słownika z poprawnym formatem
-#     else:
-#         return jsonify({"error": "Unable to fetch historical data"}), 500
-#
-# if __name__ == '__main__':
-#     app.run(debug=True)
-
 from flask import Flask, jsonify, request
 from fetcher import Fetcher
+import json
+import os
+import pandas as pd
 
 app = Flask(__name__)
 
+
+def load_symbols_from_json():
+    with open('symbol-scrapper/symbols.json', 'r') as f:
+        return json.load(f)
+
+
+def format_symbol_for_yahoo(symbol):
+    return symbol.replace('.', '-')
+
+
 @app.route('/historical_data', methods=['GET'])
 def get_historical_data():
-    # Możliwość podania wielu tickerów lub użycie domyślnej listy
-    tickers = (request.args.getlist('tickers')  or ['AAPL', 'TSLA', 'GOOGL', 'MSFT', 'AMZN'])  # Możesz dodać więcej firm
-    period = request.args.get('period', default='1y', type=str)
+    if os.path.exists('symbol-scrapper/symbols.json'):
+        tickers = load_symbols_from_json()
+    else:
+        tickers = ['AAPL', 'TSLA', 'GOOGL', 'MSFT', 'AMZN']
+
+    tickers = request.args.getlist('tickers') or tickers
+    period = request.args.get('period', default='1mo', type=str)
     interval = request.args.get('interval', default='1d', type=str)
 
     result = {}
     for ticker in tickers:
-        fetcher = Fetcher(ticker)
+        formatted_ticker = format_symbol_for_yahoo(ticker)
+        fetcher = Fetcher(formatted_ticker)
         data = fetcher.get_historical_data(period=period, interval=interval)
 
         if data is not None:
-            # Konwersja indeksu (Timestamp) na string, aby uniknąć błędów
+            if not pd.api.types.is_datetime64_any_dtype(data.index):
+                data.index = pd.to_datetime(data.index)
+
             data.index = data.index.strftime('%Y-%m-%d')
             result[ticker] = data.to_dict(orient='index')
         else:
             result[ticker] = {"error": f"Unable to fetch historical data for {ticker}"}
 
-    return jsonify(result)  # Zwrócenie danych dla wszystkich firm
+    return jsonify(result)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
